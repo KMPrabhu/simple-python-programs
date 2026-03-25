df11a035d4c8eb5974085954eef54c17620aa38a
KMPrabhu <prabhukm30@gmail.com>
2026-03-25 22:08:59 +0530

    Implement singly linked list with insert and display

---
+++ singly linked list.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLL:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new = Node(data)
+        if not self.head:
+            self.head = new
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+ll = SinglyLL()
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+ll.display()
